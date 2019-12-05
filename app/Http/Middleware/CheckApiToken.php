59eb8510c84d6aa94f9b73755454c346d36e1446
Alex Roan <alex.roan@hotmail.com>
2019-12-05 15:10:08 +0000

    Added Api-Token auth to subscribe endpoint

---
+++ app/Http/Middleware/CheckApiToken.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Support\Facades\Response;
+
+class CheckApiToken
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if(!isset(\getallheaders()['Api-Token'])){
+            return Response::json(array('error'=>'Please set api token'));  
+        }  
+  
+        if(\getallheaders()['Api-Token'] != \getenv('API_TOKEN')){  
+            return Response::json(array('error'=>'wrong api token'));  
+        }  
+        return $next($request);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -17,4 +17,7 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('/subscribe', 'SubscribeController@subscribe');
+Route::post('/subscribe', array(
+    'uses' => 'SubscribeController@subscribe',
+    'middleware' => ['apitoken']
+));
