63eed29c81cf505fba60b4df6d6e58861c476e31
Alex Roan <alex.roan@hotmail.com>
2019-12-30 17:21:44 +0000

    Added email verification

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -28,7 +28,7 @@ class HomeController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('auth');
+        $this->middleware(['auth', 'verified']);
         $this->mailchimpAccessor = app(MailchimpDataAccessor::class);
         $this->formatter = app(Formatter::class);
     }
