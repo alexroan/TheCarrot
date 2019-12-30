7934f805eef7c5bba14ab9ea1305dd2486f70c01
Alex Roan <alex.roan@hotmail.com>
2019-12-30 17:43:26 +0000

    Added beta in navbar once logged in

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,7 +21,7 @@
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
                     <img src="{{ asset('images/carrot.png') }}" alt="Signup Carrot">
-                    {{ config('app.name', 'Signup Carrot') }}
+                    {{ config('app.name', 'Signup Carrot') }} {{ __('Beta')}}
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
