368448fb5dba3608c695744ffe86af8ffbf3e9f2
Alex Roan <alex.roan@hotmail.com>
2020-01-07 16:02:30 +0000

    Added test for deleting generated files

--- tests/Unit/Carrots/Utils/FilesTest.php
+++ tests/Unit/Carrots/Utils/FilesTest.php
@@ -30,11 +30,49 @@ function file_put_contents(string $filepath, string $contents)
     return ($filepath . $contents);
 }
 
+/**
+ * Override system function
+ *
+ * @param string $pattern
+ * @return array
+ */
+function glob(string $pattern) {
+    return [
+        "1",
+        "2",
+        "3",
+        "4"
+    ];
+}
+
+/**
+ * Override system function
+ *
+ * @param string $file
+ * @return boolean
+ */
+function is_file(string $file) {
+    return true;
+}
+
+/**
+ * Override system function
+ *
+ * @param string $file
+ * @return void
+ */
+function unlink(string $file) {
+    return true;
+}
+
+use Mockery;
 use Tests\TestCase;
 
 class FilesTest extends TestCase
 {
     private $files;
+
+    private $environmentCheck;
 
     /**
      * Setup
@@ -44,7 +82,24 @@ class FilesTest extends TestCase
     public function setUp() : void
     {
         parent::setUp();
+        
+        $this->environmentCheck = Mockery::mock(EnvironmentCheck::class);
+        $this->app->instance(EnvironmentCheck::class, $this->environmentCheck);
+        
         $this->files = new Files();
+    }
+
+    /**
+     * Test that deleting generated files works
+     *
+     */
+    public function testDeleteGeneratedFiles()
+    {
+        $this->environmentCheck->shouldReceive('isDev')
+            ->once()
+            ->andReturn(true);
+
+        $this->files->deleteGeneratedFiles();
     }
 
     /**
