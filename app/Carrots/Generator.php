e21d1f1868b7f6c008bb90ffe856e3fbebdb9ef1
Alex Roan <alex.roan@hotmail.com>
2020-01-09 16:17:56 +0000

    Generating selects also

--- app/Carrots/Generator.php
+++ app/Carrots/Generator.php
@@ -26,6 +26,7 @@ class Generator
         $this->files = app(Files::class);
     }
 
+
     public function generateCarrotHtml($carrot)
     {
         $html = $this->files->readHtmlTemplate();
@@ -33,27 +34,48 @@ class Generator
 
         //{{product-image}}
         $html = str_replace("{{product-image}}", $product->image, $html);
+
         //{{h1}}
         $html = str_replace("{{h1}}", $carrot->title, $html);
+
         //{{h2}}
         $html = str_replace("{{h2}}", $carrot->subtitle, $html);
+
         //{{product-options}}
         $products = $this->productAccessor->getProducts();
         $productsHtml = '';
-        // <option data-image="http://modalgenerator.local/keyring-orange.png" value="2">Orange</option>
         foreach ($products as $product) {
             $selected = ($product->id == $carrot->product_id ? 'selected' : '');
-            $option = '<option ' . $selected . ' data-image="' . $product->image . '" value="' . $product->product_id . '">' . $product->name . '</option>';
+            $option = '<option ' . $selected . ' data-image="' . $product->image . '" value="' . $product->product_id . '">' . $product->name . '</option>\n';
             $productsHtml = $productsHtml . $option;
         }
         $html = str_replace("{{product-options}}", $productsHtml, $html);
+
         //{{merge-fields}}
         $mergeFields = $this->mailchimpAccessor->getMergeFields($carrot->mailchimp_list_id);
         $mergeFieldsHtml = '';
         foreach ($mergeFields as $field) {
-            //TODO
+            //if has choices, make select
+            $fieldHtml = '<div class="form-group">\n';
+            $id = "MERGE||" . $field->tag;
+            if (count($field->choices) > 0) {
+                $fieldHtml .= '<select required name="' . $id . '" id="' . $id . '" class="form-control">\n';
+                    $fieldHtml .= '<option selected disabled>' . $field->name . '</option>\n';
+                    foreach ($field->choices as $choice) {
+                        $fieldHtml .= '<option value="' . $choice->value . '">' . $choice->value . '</option>\n';
+                    }
+                $fieldHtml .= '</select>\n';
+            }
+            //if else text box
+            else {
+                $fieldHtml .= '<input required class="form-control" type="text" name="' . $id . '" id="' . $id . '" placeholder="' . $field->name . '">\n';
+            }
+            $fieldHtml .= '</div>\n';
+            //TODO number, text, checkbox, etc - All mailchimp supported items
+            $mergeFieldsHtml .= $fieldHtml;
         }
         $html = str_replace("{{merge-fields}}", $mergeFieldsHtml, $html);
+
         //{{button-style}}
         $buttonStyle = "background-color: " . $product->colour_code;
         $html = str_replace("{{button-style}}", $buttonStyle, $html);
@@ -61,6 +83,7 @@ class Generator
         $filename = $carrot->id . '.html';
         return $this->files->putHtmlFile($filename, $html);
     }
+
 
     public function compileCarrotJs(int $carrotId, string $htmlFile)
     {
