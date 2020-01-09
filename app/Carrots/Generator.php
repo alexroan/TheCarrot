e4ec4dfcb166531b1fcbee44fbf74400f74f5584
Alex Roan <alex.roan@hotmail.com>
2020-01-09 15:40:33 +0000

    Added products display to popup generator

--- app/Carrots/Generator.php
+++ app/Carrots/Generator.php
@@ -29,7 +29,6 @@ class Generator
     public function generateCarrotHtml($carrot)
     {
         $html = $this->files->readHtmlTemplate();
-
         $product = $this->productAccessor->getProduct($carrot->product_id);
 
         //{{product-image}}
@@ -41,8 +40,11 @@ class Generator
         //{{product-options}}
         $products = $this->productAccessor->getProducts();
         $productsHtml = '';
+        // <option data-image="http://modalgenerator.local/keyring-orange.png" value="2">Orange</option>
         foreach ($products as $product) {
-            //TODO
+            $selected = ($product->id == $carrot->product_id ? 'selected' : '');
+            $option = '<option ' . $selected . ' data-image="' . $product->image . '" value="' . $product->product_id . '">' . $product->name . '</option>';
+            $productsHtml = $productsHtml . $option;
         }
         $html = str_replace("{{product-options}}", $productsHtml, $html);
         //{{merge-fields}}
@@ -70,60 +72,5 @@ class Generator
         $js = $htmlJs . $js;
         $filename = $carrotId . '.js';
         return $this->files->putCompiledJsFile($filename, $js);
-    }
-
-    /**
-     * Create the scripts for the partner
-     *
-     * @param  Object $carrot - Recrod from the database
-     * @return string path to new file
-     */
-    public function generate($carrot)
-    {
-        //get merge fields
-        $mergeFields = $this->mailchimpAccessor->getMergeFields($carrot->mailchimp_list_id);
-        $formattedMergeFields = $this->formatter->formatMergeFields($mergeFields);
-        $mergeFieldJavascript = "const MERGE_FIELDS = " . $formattedMergeFields . ";\n";
-
-        //get products
-        $products = $this->productAccessor->getProducts();
-        $formattedProducts = $this->formatter->formatProducts($products);
-        $productsJavascript = "const PRODUCTS = " . $formattedProducts . ";\n";
-
-        //get discount code
-        $discountCodeRow = $this->carrotAccessor->getDiscountCode($carrot->id);
-        $discountJavascript = "window.discountCode = \"" . $discountCodeRow->code . "\";\n";
-
-        //got title
-        $titleJavascript = "const TITLE = \"" . $carrot->title . "\";\n";
-
-        //got subtitle
-        $subtitleJavascript = "const SUBTITLE = \"" . $carrot->subtitle . "\";\n";
-
-        //got carrotId
-        $carrotIdJavascript = "window.carrotId = \"" . $carrot->id . "\";\n";
-
-        //get the index of the product selected
-        $keyringIndexJavascript = "const SELECTED_KEYRING_ID = " . $carrot->product_id . ";\n";
-        $urls = "const ROOT_URL = '" . config('app.url') . "';
-            window.impressionUrl = ROOT_URL + '/api/impression';
-            window.subscribeUrl = ROOT_URL + '/subscribe';\n";
-
-        //get base file
-        $contents = $this->files->readBaseFile();
-
-        //prepend data to beginning
-        $contents = $titleJavascript
-            . $subtitleJavascript
-            . $mergeFieldJavascript
-            . $keyringIndexJavascript
-            . $productsJavascript
-            . $carrotIdJavascript
-            . $discountJavascript
-            . $urls
-            . $contents;
-
-        $newFileName = $carrot->id . '.js';
-        return $this->files->putNewFile($newFileName, $contents);
     }
 }
