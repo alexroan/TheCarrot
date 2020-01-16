df4a0b711fb96e9f7018db2508d87d496e7a50a7
Alex Roan <alex.roan@hotmail.com>
2020-01-16 11:59:30 +0000

    Daily stats gathering

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -26,6 +26,8 @@ class Kernel extends ConsoleKernel
     {
         $schedule->command('health:discounts')
                  ->hourly();
+        $schedule->command('stats:carrots')
+                ->daily();
     }
 
     /**
