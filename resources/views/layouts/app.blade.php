db3539bd8a60389af1f874b3a96cd5affe560d81
Alex Roan <alex.roan@hotmail.com>
2020-01-29 18:12:55 +0000

    Updated menu

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -67,37 +67,33 @@
                                 </li>
                             @endif
                         @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
 
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('home') }}">
-                                        {{ __('Dashboard') }}
-                                    </a>
-                                    <a class="dropdown-item" href="{{ route('profile') }}">
-                                        {{ __('Profile') }}
-                                    </a>
-                                    @php
-                                        $isAdmin = Auth::user()->admin;
-                                    @endphp
-                                    @if ($isAdmin == true)
-                                        <a class="dropdown-item" href="{{ route('admin') }}">
-                                            {{ __('Admin') }}
-                                        </a>
-                                    @endif
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
+                            @php
+                                $isAdmin = Auth::user()->admin;
+                            @endphp
+                            @if ($isAdmin == true)
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('admin') }}">{{ __('Admin') }}</a>
+                                </li>
+                            @endif
 
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                        @csrf
-                                    </form>
-                                </div>
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('home') }}">{{ __('Dashboard') }}</a>
                             </li>
+
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('profile') }}">{{ __('Profile') }}</a>
+                            </li>
+
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('logout') }}"
+                                    onclick="event.preventDefault();
+                                        document.getElementById('logout-form').submit();">{{ __('Logout') }}</a>
+                            </li>
+
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                @csrf
+                            </form>
                         @endguest
                     </ul>
                 </div>
