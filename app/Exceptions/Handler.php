e0de42216009314d3077075de2bc24015dcd529d
Alex Roan <alex.roan@hotmail.com>
2020-01-29 19:08:08 +0000

    Email sent when exception occurs

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,8 +2,13 @@
 
 namespace App\Exceptions;
 
+use App\Mail\ExceptionOccured;
 use Exception;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Mail;
+use Symfony\Component\Debug\Exception\FlattenException;
+use Symfony\Component\Debug\ExceptionHandler as SymfonyExceptionHandler;
 
 class Handler extends ExceptionHandler
 {
@@ -34,7 +39,33 @@ class Handler extends ExceptionHandler
      */
     public function report(Exception $exception)
     {
+        if ($this->shouldReport($exception)) {
+            $this->sendEmail($exception); // sends an email
+        }
+
         parent::report($exception);
+    }
+
+    /**
+     * Send email
+     *
+     * @param Exception $exception
+     */
+    private function sendEmail(Exception $exception)
+    {
+        if (App::environment('local')) {
+            return;
+        }
+        $error = FlattenException::create($exception);
+        $handler = app(SymfonyExceptionHandler::class);
+        $html = $handler->getHtml($error);
+        $to = [
+            [
+                'email' => config('mail.from.address'),
+                'name' => 'Signup Carrot'
+            ]
+        ];
+        Mail::to($to)->send(new ExceptionOccured($html));
     }
 
     /**
